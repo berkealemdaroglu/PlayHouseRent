5f780f8f4a0a0b5b6cfa8633a217c25591d09f97
berkealemdaroglu <ersinberkealemdaroglu@gmail.com>
2022-09-14 23:36:01 +0300

    LoginFragment Design Completed.

--- app/src/main/java/com/ersinberkealemdaroglu/playhouserent/ui/LandlordSignUpContinuedFragment.kt
+++ app/src/main/java/com/ersinberkealemdaroglu/playhouserent/ui/LandlordSignUpContinuedFragment.kt
@@ -7,28 +7,28 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.ersinberkealemdaroglu.playhouserent.R
-import com.ersinberkealemdaroglu.playhouserent.databinding.FragmentGetStartedBinding
+import com.ersinberkealemdaroglu.playhouserent.databinding.FragmentLandlordSignUpContinuedBinding
 
-class GetStartedFragment : Fragment() {
-    private lateinit var binding: FragmentGetStartedBinding
+class LandlordSignUpContinuedFragment : Fragment() {
+    private lateinit var binding: FragmentLandlordSignUpContinuedBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View {
-        binding = FragmentGetStartedBinding.inflate(inflater)
+        binding = FragmentLandlordSignUpContinuedBinding.inflate(inflater)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        getStartedButtonClick()
+        landlordSignUpCompletedButton()
     }
 
-    private fun getStartedButtonClick() {
-        binding.getStartedButton.setOnClickListener {
-            findNavController().navigate(R.id.action_getStartedFragment_to_loginFragment)
+    private fun landlordSignUpCompletedButton() {
+        binding.button.setOnClickListener {
+            findNavController().navigate(R.id.action_landlordSignUpContinuedFragment_to_homeFragment)
         }
     }
 
--- app/src/main/java/com/ersinberkealemdaroglu/playhouserent/ui/LoginFragment.kt
+++ app/src/main/java/com/ersinberkealemdaroglu/playhouserent/ui/LoginFragment.kt
@@ -1,20 +1,88 @@
 package com.ersinberkealemdaroglu.playhouserent.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.ersinberkealemdaroglu.playhouserent.R
+import com.ersinberkealemdaroglu.playhouserent.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
+    private lateinit var binding: FragmentLoginBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+    ): View {
+        binding = FragmentLoginBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        loginStatusChange()
+        loginController()
+    }
+
+
+    private fun loginStatusChange() {
+
+        binding.landlordImageButton.setOnClickListener {
+            if (binding.landlordImageButton.isPressed) {
+                binding.apply {
+                    tenantButton.visibility = View.INVISIBLE
+                    tenantText.visibility = View.INVISIBLE
+                    tenantText2.visibility = View.VISIBLE
+                    tenantButton2.visibility = View.VISIBLE
+
+                    //landlord
+                    landlordImageButton.visibility = View.INVISIBLE
+                    landlordText.visibility = View.INVISIBLE
+                    landlordImageButton2.visibility = View.VISIBLE
+                    landlordText2.visibility = View.VISIBLE
+
+                    //button
+                    binding.tenantLogic.visibility = View.INVISIBLE
+                    binding.landlordLogic.visibility = View.VISIBLE
+                }
+            }
+        }
+
+        binding.tenantButton2.setOnClickListener {
+            if (binding.tenantButton2.isPressed) {
+                binding.apply {
+                    tenantButton.visibility = View.VISIBLE
+                    tenantText.visibility = View.VISIBLE
+                    tenantText2.visibility = View.INVISIBLE
+                    tenantButton2.visibility = View.INVISIBLE
+
+                    //landlord
+                    landlordImageButton.visibility = View.VISIBLE
+                    landlordText.visibility = View.VISIBLE
+                    landlordImageButton2.visibility = View.INVISIBLE
+                    landlordText2.visibility = View.INVISIBLE
+
+                    //button
+                    binding.tenantLogic.visibility = View.VISIBLE
+                    binding.landlordLogic.visibility = View.INVISIBLE
+                }
+            }
+        }
+    }
+
+    private fun loginController() {
+
+        binding.landlordLogic.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_landlordSignUpContinuedFragment)
+        }
+
+        binding.tenantLogic.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_homeFragment)
+        }
+
     }
 
 }
